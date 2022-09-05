c50fcf9ae22f135f81e7f29f52a6c83f95a7e584
Guilherme Castro <guilherme.castro.souza1999@gmail.com>
2022-09-05 19:54:46 -0400

    adicionando hotel e verbos http

--- resources/hotel.py
+++ resources/hotel.py
@@ -35,3 +35,20 @@ hoteis = [
 class Hoteis(Resource):
     def get(self):
         return {'hoteis': hoteis}
+
+
+class Hotel(Resource):
+    def get(self, hotel_id):
+        for hotel in hoteis:
+            if hotel['hotel_id'] == hotel_id:
+                return hotel
+        return {'message': 'Hotel not found.'}, 404 #not found ou não encontrado
+
+    def post(self, hotel_id):
+        pass
+
+    def put(self, hotel_id):
+        pass
+
+    def delete(self, hotel_id):
+        pass
