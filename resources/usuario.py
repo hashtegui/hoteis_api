8d0dcd9cfcf194671642cae65a1198d9f58bd499
Guilherme Castro <guilherme.castro.souza1999@gmail.com>
2022-09-09 19:55:13 -0400

    criando UserRegister para cadastro de usuarios

--- resources/usuario.py
+++ resources/usuario.py
@@ -1,4 +1,4 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 from models.usuario import UserModel
 
@@ -16,3 +16,18 @@ class User(Resource):
             user.delete_user()
             return {'message': 'User deleted.'}
         return {'message': 'User not found.'}, 404
+
+
+class UserRegister(Resource):
+    def post(self):
+        atributos = reqparse.RequestParser()
+        atributos.add_argument('login', type=str, required=True, help="The field 'login' cannot be left empty")
+        atributos.add_argument('senha', type=str, required=True, help="The field 'senha' cannot be left empty")
+        dados = atributos.parse_args()
+
+        if UserModel.find_by_login(dados['login']):
+            return {'message': f"The {dados['login']} already exists"}
+
+        user = UserModel(**dados)
+        user.save_user()
+        return {'message': "User created successfully"}, 201
