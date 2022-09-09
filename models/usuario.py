88021b86aed1966e58d348253f93f56cf03ba8ad
Guilherme Castro <guilherme.castro.souza1999@gmail.com>
2022-09-09 19:55:49 -0400

    criando metodo find_by_login

--- models/usuario.py
+++ models/usuario.py
@@ -15,12 +15,19 @@ class UserModel(banco.Model):
     def json(self):
         return {
             'user_id': self.user_id,
-            'login': self.senha
+            'login': self.login
         }
 
     @classmethod
     def find_user(cls, user_id):
         user = cls.query.filter_by(user_id=user_id).first()
+        if user:
+            return user
+        return None
+
+    @classmethod
+    def find_by_login(cls, login):
+        user = cls.query.filter_by(login=login).first()
         if user:
             return user
         return None
