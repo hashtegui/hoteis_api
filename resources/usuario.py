a9e43b0871cb9d001e104e9dbf38083c66a89061
Guilherme Castro <guilherme.castro.souza1999@gmail.com>
2022-09-10 12:46:46 -0400

    tornando atributos globais e criando UserLogin

--- resources/usuario.py
+++ resources/usuario.py
@@ -1,6 +1,11 @@
 from flask_restful import Resource, reqparse
-
 from models.usuario import UserModel
+from flask_jwt_extended import create_access_token
+
+
+atributos = reqparse.RequestParser()
+atributos.add_argument('login', type=str, required=True, help="The field 'login' cannot be left empty")
+atributos.add_argument('senha', type=str, required=True, help="The field 'senha' cannot be left empty")
 
 
 class User(Resource):
@@ -20,9 +25,6 @@ class User(Resource):
 
 class UserRegister(Resource):
     def post(self):
-        atributos = reqparse.RequestParser()
-        atributos.add_argument('login', type=str, required=True, help="The field 'login' cannot be left empty")
-        atributos.add_argument('senha', type=str, required=True, help="The field 'senha' cannot be left empty")
         dados = atributos.parse_args()
 
         if UserModel.find_by_login(dados['login']):
@@ -31,3 +33,16 @@ class UserRegister(Resource):
         user = UserModel(**dados)
         user.save_user()
         return {'message': "User created successfully"}, 201
+
+
+class UserLogin(Resource):
+    @classmethod
+    def post(cls):
+        dados = atributos.parse_args()
+
+        user = UserModel.find_by_login(dados['login'])
+
+        if user and user.senha == dados['senha']:
+            token_de_acesso = create_access_token(identity=user.user_id)
+            return {'acess_token': token_de_acesso}
+        return {'message': 'The username or password is incorrect'}, 401
